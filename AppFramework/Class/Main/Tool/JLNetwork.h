9494ee066b02d5de98972b08b4f65307ded2192a
gaojianlong <jianlong@gao>
2017-04-11 16:21:37 +0800

    添加网络请求工具类

---
+++ AppFramework/Class/Main/Tool/JLNetwork.h
@@ -0,0 +1,13 @@
+//
+//  JLNetwork.h
+//  AppFramework
+//
+//  Created by gaojianlong on 2017/4/11.
+//  Copyright © 2017年 JLB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLNetwork : NSObject
+
+@end
---
+++ AppFramework/Class/Main/Tool/JLNetwork.m
@@ -0,0 +1,15 @@
+//
+//  JLNetwork.m
+//  AppFramework
+//
+//  Created by gaojianlong on 2017/4/11.
+//  Copyright © 2017年 JLB. All rights reserved.
+//
+
+#import "JLNetwork.h"
+
+@implementation JLNetwork
+
+
+
+@end
